e3ff5a3793d7f72f67353f29a7d0b62d233d50e5
DengLinJie <46638260+DengLinJie@users.noreply.github.com>
2019-03-25 18:16:29 +0800

    项目1
    项目

---
+++ 水炸的项目1/Assets/C#/playermovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+     
+    public float runspeed = 40f;
+
+    float HorizontalMove = 0f;
+    bool jump = false;
+    bool attack = false;
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+        animator.SetFloat("Speed",Mathf.Abs( HorizontalMove));
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJump", true);
+        }
+       
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("IsJump", false);
+
+    }
+    void FixedUpdate()
+    {
+        //move character
+        controller.Move(HorizontalMove * Time.fixedDeltaTime,false,jump);
+        jump = false;
+    }
+}
